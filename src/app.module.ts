37250afae59e38ec98cd11599c38ea92fcf37fc4
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-05-26 11:40:11 -0300

    feat(cats): simple crud generate by nest g resource cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { UserController } from './user/user.controller';
+import { UsersModule } from './modules/users/users.module';
+import { CatsModule } from './modules/cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CatsModule, UsersModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
